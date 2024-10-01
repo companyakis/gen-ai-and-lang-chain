0d504922c3692c0925e45c627dbb0bdf8cc78540
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-01 10:41:22 +0300

    Create 0101-pandas-embedding1.py

---
+++ 0101-pandas-embedding1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv("fakedata.csv")
+
+print(df)
+
+"""
+    id                  review                                  final
+0  101      servise is too bad                    I'll never go again
+1  108               i like it                   yes I'll visit again
+2  146  the place is too dirty   disgusting place... I won't go again
+
+"""
